c1400c34cb392bef2f8071509a3dd0cc184833d6
MenelikMulima <mayabamulima147@gmail.com>
2026-02-14 07:49:36 +0200

    created ATM class with methods for logging in, depositing, checking the balance for an account, withdrawing, adding a account and logging out

--- src/main/java/aCompany/ATM.java
+++ src/main/java/aCompany/ATM.java
@@ -1,4 +1,37 @@
 package aCompany;
 
 public class ATM {
+
+    Account account = new Account("12345", "0000", 500);;
+    private boolean isLoggedIn = true;
+
+    public Account addAccount(Account account){
+        return account;
+    }
+
+
+    public boolean login(String accNum, String pin) {
+        if(!account.getAccountNumber().equals(accNum) || !account.getPin().equals(pin)){
+            System.out.println("Incorrect log in details");
+            return false;
+
+        }
+        return isLoggedIn;
+    }
+
+
+    public double deposit(double amount) {
+        return account.deposit(amount);
+    }
+
+    public int checkBalance() {
+        return (int) account.getBalance();
+    }
+
+    public double withdraw(double amount) {
+        return account.withdraw(amount);
+    }
+
+    public void logout() {
+    }
 }
